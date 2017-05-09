3a13bad3f932593fb64fa15068a2e2fd48d33551
Ashok <coderatlabs@gmail.com>
2017-05-09 15:50:12 +0530

    add state to menu bar

--- components/popular.js
+++ components/popular.js
@@ -1,33 +1,47 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+const SelectLanguage = props => {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'Css', 'Python']
+
+  return (<ul className="languages">{ languages.map(lang => (
+    <li
+      style={lang === props.selectedLang ? {color: 'red'}: null}
+      onClick={() => props.handleUpdateLang(lang)}
+      key={ lang }
+    >
+      {lang}
+    </li>
+  )) }
+</ul>)
+}
+
+SelectLanguage.propTypes = {
+  selectedLang: PropTypes.string.isRequired,
+  handleUpdateLang: PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state = {
-      'selectedLanguage': 'All'
+      'selectedLang': 'All'
     }
+
+    this.handleUpdateLang = this.handleUpdateLang.bind(this)
   }
 
-  updateLanguage(lang) {
+  handleUpdateLang(lang) {
     this.setState({
-      'selectedLanguage': lang
+      'selectedLang': lang
     })
   }
 
   render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'Css', 'Python']
 
-    return (
-      <ul className="languages">{ languages.map(lang => (
-        <li
-          style={lang === this.state.selectedLanguage ? {color: 'red'}: null}
-          onClick={() => this.updateLanguage(lang)}
-          key={ lang }
-        >
-          {lang}
-        </li>
-      )) }
-    </ul>
-    )
+    return (<SelectLanguage
+      selectedLang={this.state.selectedLang}
+      handleUpdateLang={this.handleUpdateLang}
+    />)
   }
 }
