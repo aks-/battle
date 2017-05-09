cfa961fcc08b4143f119ffefba0d31fd51d5ee1b
Ashok <coderatlabs@gmail.com>
2017-05-09 14:20:59 +0530

    reactor into components

--- app/index.js
+++ app/index.js
@@ -1,13 +1,8 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
-require('./index.css')
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>Hello World!</div>
-    )
-  }
-}
+require('./index.css')
+import App from '../components/App'
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
